c8a0be108f3b63100858b778fe268d5acf198057
stefan muto <mutobui@gmail.com>
2024-07-10 11:57:13 +0700

    update

---
+++ scripts/arguments.ts
@@ -0,0 +1,5 @@
+import {byteArray} from 'starknet';
+
+console.log("Name: ",  byteArray.byteArrayFromString("OpenMark NFT"));
+console.log("Symbol: ",  byteArray.byteArrayFromString("OM"));
+console.log("Base URI: ",  byteArray.byteArrayFromString("https://nft-api.openmark.io/"));
